5babe227a855e9a8e7daeb177263a85e2d38ad55
anderson-souz4 <andersondesouzalopes69@gmail.com>
2022-09-20 17:30:47 -0300

    Implementação do LOGIN 7:39

---
+++ src/main/java/io/github/andersev7en/ClientesApplication/model/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package io.github.andersev7en.ClientesApplication.model.repository;
+
+import io.github.andersev7en.ClientesApplication.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
+}
